4631e20b7a5c4313d079ddec75b36f1b3f35be62
adithn04 <adithyan1912@gmail.com>
2025-08-01 13:46:33 +0530

    Assignment 6 --> completed

---
+++ C2TC_Assignment/src/Assignment6/InsufficientFundsException.java
@@ -0,0 +1,10 @@
+package Assignment6;
+
+
+	public class InsufficientFundsException extends Exception {
+	    public InsufficientFundsException(String message) {
+	        super(message);
+	    }
+	
+
+}
